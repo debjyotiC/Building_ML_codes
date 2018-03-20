741e49c7ff3c32e465e19f99e40459c6c2a58d8a
djbabai <djbabai.debjyoti@gmail.com>
2018-03-20 16:37:34 +0530

    updated test_ML code

--- Ch01/test_ML.py
+++ Ch01/test_ML.py
@@ -1,18 +1,37 @@
 import scipy as sp
 import matplotlib.pyplot as plt
 
+def error(f,x,y):
+    return sp.sum((f(x)-y)**2)
+
 data = sp.genfromtxt('web_traffic.tsv',delimiter = "\t")
 x = data[:,0]
 y = data[:,1]
 
-fp1 = sp.polyfit(x,y,1)
-f1 = sp.poly1d(fp1)
+inflection = 3.5*7*24
+
+#before inflection
+xa = x[:inflection]
+ya = y[:inflection]
+
+#after inflection
+xb = x[inflection:]
+yb = y[inflection:]
+
+fpa1 = sp.polyfit(xa,ya,1)
+fa1 = sp.poly1d(fpa1)
+
+fpb1 = sp.polyfit(xb,yb,1)
+fb1 = sp.poly1d(fpb1)
 
 fx = sp.linspace(0,x[-1],1000)
 
+print(error(fa1,x,y))
 
-plt.plot(fx,f1(fx),linewidth = 4)
-plt.legend(["d=%i" %f1.order],loc="upper left")
+plt.plot(fx,fa1(fx),linewidth = 4)
+plt.plot(fx,fb1(fx),linewidth = 4)
+plt.legend(["d=%i" %fa1.order],loc="upper left")
+
 plt.scatter(x,y)
 plt.xlabel("Time")
 plt.ylabel("Hits/hour")
@@ -20,3 +39,4 @@ plt.xticks([w*7*24 for w in range(10)],['week %i' %w for w in range(10)])
 plt.autoscale(tight=True)
 plt.grid()
 plt.show()
+
